8d6ce0b1bdfa3396857049c99bc1f4b4ffccfc84
Avi Sharvit <asharvit@redhat.com>
2019-03-28 19:36:27 +0200

    Fixes #26406 - Tasks dashboard state

---
+++ webpack/ForemanTasks/Components/TasksDashboard/Components/TasksCardsGrid/Components/ScheduledTasksCard/ScheduledTasksCard.stories.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import { withKnobs, number, select } from '@storybook/addon-knobs';
+import { action } from '@storybook/addon-actions';
+import { withCardsDecorator } from '../../../../../../../stories/decorators';
+
+import { TASKS_DASHBOARD_AVAILABLE_QUERY_STATES } from '../../../../TasksDashboardConstants';
+import ScheduledTasksCard from './ScheduledTasksCard';
+
+storiesOf('TasksDashboard/TasksCardsGrid', module)
+  .addDecorator(withKnobs)
+  .addDecorator(withCardsDecorator)
+  .add('ScheduledTasksCard', () => {
+    const selectState = select(
+      'query.state',
+      { ...TASKS_DASHBOARD_AVAILABLE_QUERY_STATES, NONE: 'none' },
+      'none'
+    );
+    return (
+      <div>
+        <link
+          href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"
+          rel="stylesheet"
+          integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN"
+          crossOrigin="anonymous"
+        />
+        <ScheduledTasksCard
+          data={number('data', 1)}
+          query={{
+            state: selectState,
+          }}
+          updateQuery={action('updateQuery')}
+        />
+      </div>
+    );
+  });
